94ec9298d7e730f5830c1309be94e0aa71a6312a
Liubov <pandora1705@mail.ru>
2022-09-18 14:10:19 +0400

    ДЗ Pytnon 5

--- 4.py
+++ 4.py
@@ -1,3 +1,13 @@
 # Задана натуральная степень k. Сформировать случайным образом список коэффициентов (значения от 0 до 100) многочлена и 
 # записать в файл многочлен степени k.
 # - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
+
+k = int(input("Введите степень: "))
+import numpy as np
+import random as rnd
+my_list = []
+for i in  range(k+1):
+    my_list.append(rnd.randint(0, 100))
+with open('file2.txt', 'w') as data:
+    data.write(str(np.poly1d(my_list)))
+print(np.poly1d(my_list))
